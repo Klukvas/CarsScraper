ca5911a2c29c633b1ed9e4e7e235f2a55278df11
Klukvas <pavlenkoandrey56@gmail.com>
2023-06-08 14:40:12 +0300

    changed depend manager

--- src/utils/DependencyManager.py
+++ src/utils/DependencyManager.py
@@ -35,77 +35,81 @@ class DependencyManager(metaclass=SingletonMeta):
     def __init__(self) -> None:
         self._dependencies = {}
 
-
-    def setup_dependencies(self):
-        if Env not in self._dependencies.keys():
-            env = Env()
-            self._dependencies[Env] = env
-
-        if Logger not in self._dependencies.keys():
-            logger = Logger().custom_logger(name='AutoRiaLogger')
-            self._dependencies[Logger] = logger
-
-        self.current_config = config_read(self._dependencies[Env].autoria_config_path)
-
-        db_url = build_bd_url(self._dependencies[Env])
-
-        if DatabaseClient not in self._dependencies.keys():
-            db_client = DatabaseClient(db_url)
-            self._dependencies[DatabaseClient] = db_client
-
-        if AutoQueries not in self._dependencies.keys():
-            auto_query = AutoQueries(
-                logger=self._dependencies[Logger],
-                db_client = self._dependencies[DatabaseClient]
-            )
-            self._dependencies[AutoQueries] = auto_query
-        
-        if ParametersQueries not in self._dependencies.keys():
-            parameters_queries = ParametersQueries(
-                db_client=self._dependencies[DatabaseClient],
-                logger=self._dependencies[Logger]
-            )
-            self._dependencies[ParametersQueries] = parameters_queries
-
+    @property
+    def current_config(self) -> dict:
+        return config_read(self.env.autoria_config_path)
+    
+    @property
+    def data_checker(self) -> DataChecker:
         if DataChecker not in self._dependencies.keys():
             data_checker = DataChecker(
                 params_queries=self._dependencies[ParametersQueries],
                 logger=self._dependencies[Logger]
             )
             self._dependencies[DataChecker] = data_checker
+        return self._dependencies[DataChecker]
 
+    @property
+    def logger(self) -> Logger:
+        if Logger not in self._dependencies.keys():
+            logger = Logger().custom_logger(name='AutoRiaLogger')
+            self._dependencies[Logger] = logger
+        return self._dependencies[Logger]
+
+    @property
+    def parameters_queries(self) -> ParametersQueries:
+        if ParametersQueries not in self._dependencies.keys():
+            parameters_queries = ParametersQueries(
+                db_client=self.db_client,
+                logger=self.logger
+            )
+            self._dependencies[ParametersQueries] = parameters_queries
+        return self._dependencies[ParametersQueries]
+
+    @property
+    def env(self) -> Env:
+        if Env not in self._dependencies.keys():
+            env = Env()
+            self._dependencies[Env] = env
+        return self._dependencies[Env]
+
+    @property
+    def auto_query(self) -> AutoQueries:
+        if AutoQueries not in self._dependencies.keys():
+            auto_query = AutoQueries(
+                logger=self.logger,
+                db_client=self.db_client
+            )
+            self._dependencies[AutoQueries] = auto_query
+        return self._dependencies[AutoQueries]
+
+    @property
+    def db_url(self) -> str:
+        db_url = build_bd_url(self.env)
+        return db_url
+
+    @property
+    def db_client(self) -> DatabaseClient:
+        if DatabaseClient not in self._dependencies.keys():
+            db_client = DatabaseClient(self.db_url)
+            self._dependencies[DatabaseClient] = db_client
+        return self._dependencies[DatabaseClient]
+
+    @property
+    def scrapper(self) -> Scrapper:
         if Scrapper not in self._dependencies.keys():
             scrapper = Scrapper(
                 base_url=self.base_url,
                 api_keys=self.api_keys,
                 page_count=self.page_count,
                 max_scpapped=self.max_scpapped,
-                logger=self._dependencies[Logger],
-                auto_query=self._dependencies[AutoQueries],
-                data_checker=self._dependencies[DataChecker]
+                logger=self.logger,
+                auto_query=self.auto_query,
+                data_checker=self.data_checker
             )
             self._dependencies[Scrapper] = scrapper
-    
-    @property
-    def env(self):
-        return self._dependencies[Env]
-
-    @property
-    def parameters_queries(self) -> ParametersQueries:
-        return self._dependencies[ParametersQueries]
-
-    @property
-    def db_client(self):
-        return self._dependencies[DatabaseClient]
-
-    @property
-    def scrapper(self):
         return self._dependencies[Scrapper]
     
-    @property
-    def data_checker(self) -> DataChecker:
-        return self._dependencies[DataChecker] 
-
     @property
     def api_keys(self) -> list:
         api_keys = self.current_config.get('AUTO_RIA_API_KEYS', None)
@@ -114,21 +118,21 @@ class DependencyManager(metaclass=SingletonMeta):
         return api_keys
     
     @property
-    def base_url(self):
+    def base_url(self) -> str:
         base_url = self.current_config.get('BASE_URL', None)
         if base_url is None:
             raise AttributeError(f"base_url is None")
         return base_url
     
     @property
-    def page_count(self):
+    def page_count(self) -> int:
         page_count = self.current_config.get('PAGE_COUNT', None)
         if page_count is None:
             raise AttributeError(f"page_count is None")
         return page_count
     
     @property
-    def max_scpapped(self):
+    def max_scpapped(self) -> int:
         max_scpapped = self.current_config.get('MAX_SCRAPPED', None)
         if max_scpapped is None:
             raise AttributeError(f"max_scpapped is None")
